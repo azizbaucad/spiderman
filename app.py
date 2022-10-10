22aee59a2d9401856581e1bb9eb9abd06890a3c8
Abdou Aziz Ba [67004] <abdouaziz.ba@orange-sonatel.com>
2022-10-10 15:11:29 +0000

    commit

--- app.py
+++ app.py
@@ -1,271 +1,321 @@
-import os
-
-import psycopg2
-from flask import Flask, g , jsonify, request, Config
+from flask import Flask, request, jsonify, Config
 from script.function import *
-import jsonify, json, jsonpickle
+import yact
 import requests
-from flask_cors import CORS
-from functools import wraps
-from flask_oidc import OpenIDConnect
-import logging
-import yact as yact
-
-
+import flask
 
 app = Flask(__name__)
-test = "ceci est un test()"
-#export Flask_APP =
 
-# Build custom conf object yaml conf
+
 class YactConfig(Config):
     def from_yaml(self, config_file, directory=None):
-
         config = yact.from_file(config_file, directory=directory)
         for section in config.sections:
             self[section.upper()] = config[section]
 
-def cfg():
 
+def cfg():
     with open(os.path.dirname(os.path.abspath(__file__)) + './script/config.yaml', "r") as ymlfile:
         cfg = yaml.load(ymlfile.read(), Loader=yaml.FullLoader)
         return cfg
 
-Client_d = cfg()['CLIENT_ID']
-client_secret = cfg()['CLIENT_SECRET']
-Realm = cfg()['REALM']
 
-client_test = os.getenv('CLIENT_ID')
-print("Le client secret est " + client_secret)
-print("Le client Id est " + Client_d)
+#  Les fonctions utiles
+def add_headers(response):
+    response.headers.add('Content-Type', 'application/json')
+    response.headers.add('X-Frame-Options', 'SAMEORIGIN')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+
+    return response
 
 
-
-@app.route("/")
-def home():
-    return  Client_d # "Hello Flask  on fait des tesst test"
-
-#create_table_inventaire()
-@app.route("/inventaire")
-def table_inventaire():
-    con = None
-    #response = []
-    try:
-        con = connect()
-        cur = con.cursor();
-        cur.execute("SELECT * FROM inventaireglobal_ftth ;")
-        response = cur.fetchall()
-        #print("Response data is ok ...")
-        cur.close()
-        con.commit()
-        return response
-        #print("Database closed .....")
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("The database error " + error )
-    finally:
-        if con is not None:
-            con.close()
-    #return response
-
-# simple inventaire info
-@app.route('/simpleinventaire')
-def simpleinventaire():
-    #data = []
-    con = None
-    try:
-        con = connect()
-        cur = con.cursor()
-        cur.execute("SELECT * FROM inventaireglobal_ftth ;")
-        data = cur.fetchall()
-        cur.close()
-        con.commit()
-        #return jsonpickle.encode(data)
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if con is not None:
-            con.close()
-    return jsonpickle.encode(data)
-
-# Keycloak test
-@app.route('/token_login/', methods=['POST'])
-def get_token():
-    body = request.get_json()
-    for field in ['username', 'password']:
-        if field not in body:
-
-            return  ("Error :::::::: Field {field}  is missing! , 400")
-
+# La fonction adminToken
+def adminToken():
     data = {
-        'grant_type': 'password',
-        'client_id': 'test'
+        'grant_type': 'client_credentials',
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
     }
 
-# function pour obtenir les variables d'env
+    url = URI
 
-def sysEnvOrDefault(envVar, defaultVal):
-    if os.getenv(envVar):
-        return os.getenv(envVar)
-    return defaultVal
+    response = requests.post(url, data=data)
+
+    if response.status_code > 200:
+        return {"message": "Username ou Password Incorrect", 'status': 'error'}
+    tokens_data = response.json()
+    ret = {
+        'tokens': {"access_token": tokens_data['access_token'],
+                   "token_type": tokens_data['token_type'],
+                   },
+        "status": 'success',
+    }
+    return ret
+
+
+# la fonction error
+
+def error(message):
+    return jsonify({
+        'success': False,
+        'message': message
+    })
+
+
+# la fonction create_users
+@app.route('/users/', methods=['POST'])
+def create_user():
+    try:
+        headers = flask.request.headers
+        # data_token = decodeToken(token)
+        if request.headers.get('Authorization'):
+            if request.headers.get('Authorization').startswith('Bearer'):
+                bearer = headers.get('Authorization')
+                taille = len(bearer.split())
+                if taille == 2:
+                    token = bearer.split()[1]
+                else:
+                    return {"message": "invalid token", 'code': HTTPStatus.UNAUTHORIZED}
+            else:
+                return {"message": "invalid token", 'code': HTTPStatus.UNAUTHORIZED}
+        else:
+            return {"message": "invalid token", 'code': HTTPStatus.UNAUTHORIZED}
+
+        data_token = decodeToken(token)
+        code = data_token['code']
+        name = data_token['data']['name']
+        if code == 200:
+            if getRoleToken(token) == 'admin':
+                url = URI_USER
+
+                body = request.json
+                for field in ['firstName', 'lastName', 'username', 'password']:
+                    if field not in body:
+                        return error(f"Field {field} is missing"), 400
+                data = {
+                    "firstName": body['firstName'],
+                    "lastName": body['lastName'],
+                    "enabled": "true",
+                    "username": body['username'],
+                    "credentials": [
+                        {
+                            "type": "password",
+                            "value": body['password'],
+                            "temporary": False
+
+                        }
+                    ]
+                }
+                # return data
+                donnee = adminToken()
+                headers = get_keycloak_headers()
+                response = requests.post(url, headers=headers, json=data)
+                if response.status_code == 400:
+                    return {"message": "Le nom de l'utilisateur existe deja", 'status': 'error',
+                            'code': response.status_code}
+                if response.status_code > 201:
+                    return {"message": "Erreur", 'status': 'error', 'code': response.status_code}
+                response = jsonify(
+                    {'status': 'Success', 'data': 'Utilisateur Cree avec success', 'code': HTTPStatus.OK})
+                # Mettre ici les logs
+                return add_headers(response)
+            return {"message": "invalid user", 'code': HTTPStatus.UNAUTHORIZED}
+        else:
+            return decodeToken(token)
+
+        # code = decodeToken(token)['code']
+
+    except ValueError:
+        return jsonify({'status': 'Error', 'error': ValueError})
+
+
+# creation de la fonction get_keycloak_headers
+def get_keycloak_headers():
+    donnee = adminToken()
+    token_admin = donnee['tokens']['access_token']
+    return {
+        'Authorization': 'Bearer' + token_admin,
+        'Content-type': 'application/json'
+    }
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World! WELCOME TO Saytu Network Backend'
+
+
+@app.route('/auth', methods=['POST'])
+def get_token():
+    # TODO : Control username et password
+    body = request.json
+
+    data = {
+
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "grant_type": 'password',
+        'username': body['username'],
+        'password': body['password']
+    }
+
+    url = URI
+    response = requests.post(url, data=data)
+    if response.status_code > 200:
+        messageLogging = body['username'] + "a tente de se connecter"
+        return {"message": "Username ou Password Incorrect", 'code': HTTPStatus.BAD_REQUEST}
+
+    tokens_data = response.json()
+
+    ret = {
+        'tokens': {
+            "access_token": tokens_data['access_token'],
+            "refresh_token": tokens_data['refresh_token'],
+        }
+    }
+    messageLogging = body['username'] + "s'est connecté avec success"
+
+    return jsonify({"message": "ok", "codes": 200}, ret), 200
+
+
+# La fonction get_user_by_id
+def get_user_by_id(userId):
+    try:
+        url = URI_USER + '/' + userId
+        donnee = adminToken()
+        token_admin = donnee['tokens']['access_token']
+        response = requests.get(url,
+                                headers={'Authorization': 'Bearer {}'.format(token_admin)})
+        if response.status_code > 200:
+            return {"message": "Erreur", 'status': 'error', 'code': response.status_code}
+        tokens_data = response.json()
+        data = {
+            "enabled": tokens_data['enabled'],
+            "id": tokens_data['id'],
+            "firstName": tokens_data['firstName'],
+            "lastName": tokens_data['lastName'],
+            "username": tokens_data['username'],
+        }
+        response = {'status': 'Success', 'data': data, 'code': HTTPStatus.OK}
+        return response
+    except ValueError:
+        return jsonify({'status': 'Error', 'error': ValueError})
+
+
+# la fonction get_info_user
+def get_info_user(userId):
+    try:
+        url = URI_USER + '/' + userId + '/role-mappings/realm'
+        donnee = adminToken()
+        token_admin = donnee['tokens']["access_token"]
+        response = requests.get(url,
+                                headers={'Authorization': 'Bearer {}'.format(token_admin)})
+        if response.status_code > 200:
+            return {"message": "Erreur", 'status': 'error', 'code': response.status_code}
+            tokens_data = response.json
+            data = {
+                "name": tokens_data[0]['name'],
+                "id": tokens_data[0]['id']
+            }
+            return data
+    except ValueError:
+        return jsonify({'status': 'Error', 'error': ValueError})
+
+
+# api de recupération des users
+
+@app.route('/users/', methods=['GET'])
+def all_users():
+    print(decodeToken(request.headers.get('Authorization').split()[1]))
+    #ffdecodeToken()
+    try:
+        headers = flask.request.headers
+        #print
+        if request.headers.get('Authorization'):
+            if request.headers.get('Authorization').startswith('Bearer'):
+                bearer = headers.get('Authorization')
+                taille = len(bearer.split())
+                if taille == 2:
+                    token = bearer.split()[1]
+                else:
+                    return {"message": "invalid token roel", 'code': HTTPStatus.UNAUTHORIZED}
+            else:
+                return {"message": "invalid token orl", 'code': HTTPStatus.UNAUTHORIZED}
+        else:
+            return {"message": "invalid token rlo", 'code': HTTPStatus.UNAUTHORIZED}
+
+        data = decodeToken(token)
+        code = data['code']
+        #name = data['data']['name']
+        if code == 200:
+            if getRoleToken(token) == 'admin' or getRoleToken(token) == 'sf':
+
+                url = URI_USER + '?max=1000'
+                donnee = adminToken()
+                token_admin = donnee['tokens']["access_token"]
+                response = requests.get(url,
+                                        headers={'Authorization': 'Bearer {}'.format(token_admin)})
+                if response.status_code > 200:
+                    return {"message": "Erreur", 'status': 'error', 'code': response.status_code}
+                tokens_data = response.json()
+                # return tokens_data[0]
+                result = []
+                for i in range(len(tokens_data)):
+                    data = {
+                        "enabled": tokens_data[i]['enabled'],
+                        "id": tokens_data[i]['id'],
+                        "firstName": tokens_data[i]['firstName'].split(' ', 1)[0],
+                        "lastName": tokens_data[i]['lastName'].upper(),
+                        "username": tokens_data[i]['username'],
+                        "profil": get_info_user(tokens_data[i]['id']),
+                    }
+                    result.append(data)
+                # return result
+                response = jsonify({'status': 'Success', 'data': result, 'code': HTTPStatus.OK})
+                # messageLogging = name + " a consulté la liste des utilisateurs "
+                # message_log = {
+                #     "url.path": request.base_url,
+                #     "http.request.method": request.method,
+                #     "client.ip": getIpAdress(),
+                #     "event.message": messageLogging,
+                #     "process.id": os.getpid(),
+                # }
+                # log_app(message_log)
+                # logger_user(messageLogging, LOG_AUTHENTIFICATION)
+                return add_headers(response)
+            # messageLogging = name + " a tenté de consulter la liste des utilisateurs "
+            # message_log = {
+            #     "url.path": request.base_url,
+            #     "http.request.method": request.method,
+            #     "client.ip": getIpAdress(),
+            #     "event.message": messageLogging,
+            #     "process.id": os.getpid(),
+            # }
+            # log_app(message_log)
+            # logger_user(messageLogging, LOG_AUTHENTIFICATION)
+            return {"message": "invalid user", 'code': HTTPStatus.UNAUTHORIZED}
+        else:
+            return decodeToken(token)
+    except ValueError:
+        return jsonify({'status': 'Error ', 'error': ValueError})
+
 
 # Recuperation des variables
 CLIENT_ID = cfg()['CLIENT_ID']
 CLIENT_SECRET = cfg()['CLIENT_SECRET']
 GRANT_TYPE = cfg()['GRANT_TYPE']
-#HOST = cfg()['CLIENT_ID']
-#PORT = sysEnvOrDefault("PORT", app.config['PORT'])
+# HOST = cfg()['CLIENT_ID']
+# PORT = sysEnvOrDefault("PORT", app.config['PORT'])
 URI = cfg()['URI']
 URI_USER = cfg()['URI_USER']
 URI_ROLES = cfg()['URI_ROLES']
 REALM = cfg()['REALM']
 URI_BASE = cfg()['URI_BASE']
 
-print("CLIENT_ID" + CLIENT_ID + "CLIENT_SECRET" + CLIENT_SECRET + "GRANT_TYPE" + GRANT_TYPE + "URI" + URI + "URI_USER" + URI_USER + "URI_ROLES" + URI_ROLES + "REALM" + REALM + "URI_BASE" + URI_BASE)
+print(
+    "CLIENT_ID" + CLIENT_ID + "CLIENT_SECRET" + CLIENT_SECRET + "GRANT_TYPE" + GRANT_TYPE + "URI" + URI + "URI_USER" + URI_USER + "URI_ROLES" + URI_ROLES + "REALM" + REALM + "URI_BASE" + URI_BASE)
+field = "aziz"
+print(f"error() : Field {field} is missing", 400)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# le headers
-# def add_headers(response):
-#     response.headers.add('Content-Type', 'application/json')
-#     response.headers.add('X-Frame-Options', 'SAMEORIGIN')
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     response.headers.add('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
-#     return response
-#
-# # creation des fonctions relatifs à Keycloak
-# def adminToken():
-#     data = {
-#         'grant_type': 'client_credentials',
-#         'client_id': CLIENT_ID,
-#         'client_secret': CLIENT_SECRET,
-#     }
-#     url = URI
-#     response = requests.post(url, data=data)
-#
-#     if response.status_code > 200:
-#         return {"message": "Username ou password Incorrect", 'status': 'error'}
-#     tokens_data = response.json()
-#     ret = {
-#         'tokens' : {
-#             "access_token": tokens_data['access_token'],
-#             "token_type": tokens_data['token_type'],
-#         },
-#         "status": 'success',
-#     }
-#     return ret
-# # fin de la creation des fonctions relatis à keycloak
-#
-# fonction de récupération des variables d'environnement
-# def sysEnvOrDefault(envVar, defaultVal):
-#     if os.getenv(envVar):
-#         return os.getenv(envVar)
-#     return defaultVal
-#
-# # Les fichiers de ressources
-#
-# CLIENT_ID = sysEnvOrDefault("CLIENT_ID", app.config['CLIENT_ID'])
-# CLIENT_SECRET = sysEnvOrDefault("CLIENT_SECRET", app.config['CLIENT_SECRET'])
-# OPENID_URL = sysEnvOrDefault("OPENID_URL", app.config['OPENID_URL'])
-# ADMIN_USER = sysEnvOrDefault("ADMIN_USER", app.config['ADMIN_USER'])
-# ADMIN_PASS = sysEnvOrDefault("ADMIN_PASS", app.config['ADMIN_PASS'])
-# REALM = sysEnvOrDefault("REALM", app.config['REALM'])
-# KEYCLOAK_URI = sysEnvOrDefault("KEYCLOAK_URI", app.config['KEYCLOAK_URI'])
-#
-# # Test de Keycloak
-# CORS(app)
-# SECRET_KEY = 'test_secret_key'
-#
-# app.config.update({
-#     'SECRET_KEY': SECRET_KEY,
-#     'TESTING': True,
-#     'DEBUG': True,
-#     'OIDC_CLIENT_SECRETS': 'client_secrets_templates.json',
-#     'OIDC_OPENID_REALM': 'hysds',
-#     'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post',
-#     'OIDC_TOKEN_TYPE_HINT': 'access_token',
-#     'OIDC-SCOPES': ['openid']
-# })
-
-# class NewOpenIDConnect(OpenIDConnect):
-#     def accept_token_modified(self, require_token=False, scopes_required=None, render_errors=True):
-#         def wrapper(view_func):
-#             @wraps(view_func)
-#             def decorated(*args, **kwargs):
-#                 print('HEADERS ################################"')
-#                 print(request.headers)
-#
-#                 if True:
-#                     print("Skip authenticating...")
-#                     return view_func(*args, **kwargs)
-#
-#                 print("request coming through proxy, authenticating...")
-#                 token = None
-#
-#                 if 'Authorization' in request.headers and request.headers['Authorization'].startswith('Bearer'):
-#                     token = request.headers['Authorization'].split(None, 1)[1].strip()
-#                 if 'access_token' in request.form:
-#                     token = request.form['access_token']
-#                 elif 'access_token' in request.args:
-#                     token = request.args['access_token']
-#
-#                 validity = self.validate_token(token, scopes_required)
-#                 if (validity is True) or (not require_token):
-#                     return view_func(*args, **kwargs)
-#                 else:
-#                     response_body = {
-#                         'error': 'invalid_token',
-#                         'error_description': validity
-#                     }
-#                     if render_errors:
-#                         response_body = json.dumps(response_body)
-#                     return response_body, 401, {'WWW-Authenticate': 'Bearer'}
-#                 return decorated
-#             return wrapper
-#
-# oidc = NewOpenIDConnect(app)
-#
-# @app.route('/saytutest', methods=['GET'])
-# def no_token_api():
-#     return jsonify({
-#         'results': 'No Need for token'
-#     })
-#
-# @app.route('/api/test', methods=['GET'])
-# @oidc.accept_token(require_token=True)
-# def test_token_api():
-#     print('HEADERS ###################################')
-#     print(request.headers)
-#     payload = g.oidc_token_info
-#     print(json.dumps(payload, indent=2))
-#
-#     return jsonify({ 'succes': True, 'payload': payload})
-#
-# @app.route('/api/optional', methods= ['GET'])
-# @oidc.accept_token_modified(require_token=True)
-# def test_optional_token():
-#     return jsonify({
-#         'success': True,
-#         "message": "testing the /api/optional endpoint"
-#     })
-
-
-
-
-# Executer le programme
 if __name__ == '__main__':
-    #create_table_inventaire()
     app.run(debug=True)
-
--- script/function.py
+++ script/function.py
@@ -1,61 +1,16 @@
 import os
-import yaml
-import psycopg2
-from pathlib import Path
-import random
-import shutil
-import smtplib
-import socket
-import string
-from datetime import datetime
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from http import HTTPStatus
-from pathlib import Path
+
 import jwt
-import pandas as pd
-from psycopg2 import Error
-import glob
+import yaml
 import logging
+
+from http import HTTPStatus
+import socket
+
 
 with open(os.path.dirname(os.path.abspath(__file__)) + '/config.yaml', "r") as ymlfile:
     cfg = yaml.load(ymlfile.read(), Loader=yaml.FullLoader)
 
-# function de connection à la BDD
-def connect():
-    return psycopg2.connect(database=cfg["NAME_DB"], user=cfg["USER_DB"], password=cfg["PASSWORD_DB"], host=cfg["HOST_DB"], port=cfg["PORT_DB"])
-
-# function de creation de la table inventaire
-def create_table_inventaire():
-    con = connect()
-    cursor = con.cursor()
-    try:
-        create_table_query = '''CREATE TABLE IF NOT EXISTS inventaireglobal_ftth 
-        (
-        id serial PRIMARY KEY,
-        ont_index BIGINT NOT NULL,
-        ont_id int NOT NULL,
-        service_id varchar(20) NOT NULL,
-        ip_olt varchar(20) NOT NULL,
-        slot int NOT NULL,
-        pon int NOT NULL,
-        pon_index BIGINT NOT NULL,
-        vendeur varchar(100) NOT NULL,
-        nom_olt varchar(100) NOT NULL,
-        created_at TIMESTAMP DEFAULT Now()
-        ); '''
-
-        cursor.execute(create_table_query)
-        con.commit()
-        print("Table created succesfully in Postgresql")
-    except (Exception, Error) as error:
-        print("Error while connecting to PostgreSQL", error)
-    finally:
-        if con:
-            cursor.close()
-            con.close()
-            print("Connection on Postgresql database is disconnected")
 
 # function de decodage du token avec JWT
 def decodeToken(token):
@@ -63,8 +18,9 @@ def decodeToken(token):
         decoded = jwt.decode(token, options={"verify_signature": False})
         return {"data": decoded, 'code': HTTPStatus.ok}
     except:
-        return {"message": "invalid token", 'code': HTTPStatus.UNAUTHORIZED}
+        return {"message": "invalid token : decodage", 'code': HTTPStatus.UNAUTHORIZED}
 
+# function getRoleToken pour l'attribution des roles
 # function getRoleToken pour l'attribution des roles
 def getRoleToken(token):
     try:
@@ -76,10 +32,25 @@ def getRoleToken(token):
     except ValueError:
         return {'status': 'Error', 'error': ValueError}
 
+# La fonction getIpAdress()
+def getIpAdress():
+    h_name = socket.gethostname()
+    IP_addres = socket.gethostbyname(h_name)
+    return IP_addres
 
+# La fonction log_app()
+def log_app(message):
+    file_formatter = logging.Formatter(
+        "{'time':'%(asctime)s', 'service.name': 'Diag_Distant', 'level': '%(levelname)s', 'message': " + str(
+            message) + "}"
+    )
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
 
-
-
-
-
-
+    console.setFormatter(file_formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
+    # logging.basicConfig(format='%(asctime)s %(message)s ' + message, datefmt='%d/%m/%Y %H:%M:%S')
+    # logging.StreamHandler(sys.stdout)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
